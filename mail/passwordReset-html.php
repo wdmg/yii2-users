af1a89085c2a815739dd615df9f7c0dda4d5a830
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-27 20:45:35 +0200

    Email templates

---
+++ mail/passwordReset-html.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user \wdmg\users\models\Users */
+
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl([Yii::$app->requestedRoute, 'token' => $user->password_reset_token]);
+
+?>
+<div class="password-reset">
+    <h3><?= Html::encode(Yii::t('app/modules/users', 'Hi {username}!', [
+            'username' => $user->username,
+        ])); ?></h3>
+    <p><?= Yii::t('app/modules/users', 'Follow the link below to reset your password: {link}', [
+        'link' => Html::a(Html::encode($resetLink), $resetLink),
+        ]); ?></p>
+</div>
---
+++ mail/passwordReset-text.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $user \wdmg\users\models\Users */
+
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl([Yii::$app->requestedRoute, 'token' => $user->password_reset_token]);
+
+?>
+<?= Yii::t('app/modules/users', 'Hi {username}!', [
+    'username' => $user->username,
+]); ?>
+
+<?= Yii::t('app/modules/users', 'Follow the link below to reset your password: {link}!', [
+    'link' => $resetLink,
+]); ?>

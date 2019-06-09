23f1baa7761f947cd034a04e3a7781c7ce8fd070
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-10 02:53:22 +0300

    Added silent mode to reset password model

--- models/UsersResetPassword.php
+++ models/UsersResetPassword.php
@@ -25,18 +25,25 @@ class UsersResetPassword extends Model
      *
      * @param string $token
      * @param array $config name-value pairs that will be used to initialize the object properties
+     * @param boolean $silent to check the muted mode
      * @throws InvalidArgumentException if token is empty or not valid
      */
-    public function __construct($token, $config = [])
+    public function __construct($token, $config = [], $silent = false)
     {
-        if (empty($token) || !is_string($token))
-            throw new InvalidArgumentException(Yii::t('app/modules/users', 'Password reset token cannot be blank.'));
-
+        if (empty($token) || !is_string($token)) {
+            if ($silent)
+                return false;
+            else
+                throw new InvalidArgumentException(Yii::t('app/modules/users', 'Password reset token cannot be blank.'));
+        }
 
         $this->_user = Users::findByPasswordResetToken($token);
-
-        if (!$this->_user)
-            throw new InvalidArgumentException(Yii::t('app/modules/users', 'Wrong password reset token.'));
+        if (!$this->_user) {
+            if ($silent)
+                return false;
+            else
+                throw new InvalidArgumentException(Yii::t('app/modules/users', 'Wrong password reset token.'));
+        }
 
         parent::__construct($config);
     }
@@ -50,6 +57,19 @@ class UsersResetPassword extends Model
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
         ];
+    }
+
+    /**
+     * Сhecks user is found
+     *
+     * @return boolean
+     */
+    public function userIsFound()
+    {
+        if ($this->_user)
+            return true;
+        else
+            return false;
     }
 
     /**

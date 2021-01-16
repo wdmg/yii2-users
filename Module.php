5b83a19d06cb00f5b900d33b0c67242a67539715
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2021-01-16 02:17:07 +0200

    Extended module dashboardNavItems() method

--- Module.php
+++ Module.php
@@ -92,7 +92,7 @@ class Module extends BaseModule
     /**
      * {@inheritdoc}
      */
-    public function dashboardNavItems($createLink = false)
+    public function dashboardNavItems($options = false)
     {
         $items = [
             'label' => $this->name,
